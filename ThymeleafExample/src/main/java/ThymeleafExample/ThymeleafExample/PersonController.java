fcfd214a9481fdef9d07074e0b490b36e2b104a1
ryan <ryan.hill1@mydit.ie>
2021-02-11 13:52:47 +0000

    change / to index file

--- ThymeleafExample/src/main/java/ThymeleafExample/ThymeleafExample/PersonController.java
+++ ThymeleafExample/src/main/java/ThymeleafExample/ThymeleafExample/PersonController.java
@@ -1,16 +1,13 @@
 package ThymeleafExample.ThymeleafExample;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PersonController {
 
     @GetMapping
     String getPeople(){
-        return "people";
+        return "index";
     }
 }
