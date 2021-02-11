2502c879169438d7cc7cf3f38a24d011eabdc48c
ryan <ryan.hill1@mydit.ie>
2021-02-11 16:02:41 +0000

    passing object to html page to display in table

--- ThymeleafExample/src/main/java/ThymeleafExample/ThymeleafExample/PersonController.java
+++ ThymeleafExample/src/main/java/ThymeleafExample/ThymeleafExample/PersonController.java
@@ -1,13 +1,28 @@
 package ThymeleafExample.ThymeleafExample;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Array;
+import java.util.Arrays;
 
 @Controller
 public class PersonController {
 
     @GetMapping
-    String getPeople(){
+    String getIndex(Model model){
+        model.addAttribute("title", "This is my home page");
         return "index";
     }
+
+    @GetMapping("/person")
+    String getPeople(Model model){
+        model.addAttribute("person", Arrays.asList(
+                new Person("Ryan", 23),
+                new Person("Amy", 22)
+        ));
+        return "person";
+    }
+
 }
